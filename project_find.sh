4e3d3d430cf37d0f883444ff156219d6c5e9e94f
scheleon <scheleon08@gmail.com>
2019-02-26 11:12:34 +0530

    saves list of files in /var/tmp/temp_project_find/

--- project_find.sh
+++ project_find.sh
@@ -1,62 +1,82 @@
-
 #program to return all the directories in a directory
-mkdir -p /var/tmp/temp_project_find
-if [[ -d /var/tmp/temp ]]; then
-	rm /var/tmp/temp_project_find/direct*.txt		2> /dev/null
-	rm /var/tmp/regular_files*.txt	2> /dev/null
-	rm /var/temp/temp_project_find/symbolic_links.txt  	2> /dev/null
+list-directory () {
+
+if [ ! -n "$1" ] && [ ! -d "$1" ]; then
+	echo "usage: $(basename $0) directory" >&2
+	return 1;
+fi
+
+local temp_folder
+
+temp_folder="/var/tmp/temp_project_find"
+
+mkdir -p "$temp_folder"
+if [[ -d "$temp_folder" ]]; then
+	rm "$temp_folder/direct*.txt"		2> /dev/null
+	rm "$temp_folder/regular_files*.txt"	2> /dev/null
+	rm "$temp_folder/symbolic_links.txt"  	2> /dev/null
 	 #mkdir ~/temp_project_find/	
 else
-	mkdir ~/temp_project_find/
+	echo "Error creating temporary folder needed, quitting now"
+	exit
 fi	
+
 echo "Temporary folder created! proceeding with further execution"
 
 # Funtion
+local depth=0
+
 depth=0
-prev_directory=$pwd
 cd "$1"
 depth=$((depth+1))
 (( count$depth = 0))
 
 directory_list () {
 	curr_directory=$(pwd)           # returns current directory, to further check
-	if [ -f /var/tmp/temp_project_find/directory_$depth.txt ]; then
-		rm /var/tmp/temp_project_find/directory_$depth.txt
+	if [ -f "$temp_folder/directory_$depth.txt" ]; then
+		rm "$temp_folder/directory_$depth.txt"
 	fi
 	((count$depth=0))
 	for i in * ; do
 			if [[ -L "$i" ]] ; then
-			echo "$curr_directory/$i" >> /var/tmp/temp_project_find/symbolic_links.txt		
+			echo "$curr_directory/$i" >> "$temp_folder/symbolic_links.txt"		
 			elif [[ -d "$i" ]] ; then
-			echo "$curr_directory/$i" >> /var/tmp/temp_project_find/directory_$depth.txt
+			echo "$curr_directory/$i" >> "$temp_folder/directory_$depth.txt"
 			((count$depth= $((count$depth + 1)) )) 		
 		elif [[ -f "$i" ]]; then
-			echo $curr_directory/"$i" >> /var/tmp/temp_project_find/regular_files.txt
+			echo "File $i"
+			echo "$curr_directory/$i" >> "$temp_folder/regular_files.txt"
 		fi
 	done
-	echo "Function completed"
+	# echo "Function completed"
  	return 
  	}
 #Test: required to test if variables are working as expected
 #echo $depth                  
 #echo $((count$depth))
 
-directory_list 
-echo "depth = $depth , directory :- $curr_directory count = $((count$depth))"
+directory_list
+ 
+# echo "depth = $depth ,directory :- $curr_directory count = $((count$depth))"
+
+echo "Scanning $curr_directory"
+
 while (( count1 > 0 )); do	
-	directory="$(head -1 ~/temp_project_find/directory_$depth.txt)"
+	directory="$(head -1 $temp_folder/directory_$depth.txt)"
 	if [ -n "$directory" ]; then
 		cd "$directory"
 		depth=$(( depth + 1 ))
 		directory_list	
-	echo "depth = $depth , directory :- $curr_directory count = $((count$depth))"
+	echo "Scanning $curr_directory"
 	fi
 	if (( count$depth == 0 )); then
-		echo "traversing back $directory done"		# Testing...!!!
+		# echo "traversing back $directory done"		# Testing...!!!
 		cd ..
 		depth=$(( depth - 1 ))
 		(( count$depth =  $((count$depth - 1)) )) 
-		echo "$(tail -n +2 ~/temp_project_find/directory_$depth.txt)" > /var/tmp/temp_project_find/directory_$depth.txt
+		echo "$(tail -n +2 $temp_folder/directory_$depth.txt)" > $temp_folder/directory_$depth.txt
 	fi
 done
-
+echo "Directory scan completed"
+printf "List stored in %s\n" $temp_folder
+}
